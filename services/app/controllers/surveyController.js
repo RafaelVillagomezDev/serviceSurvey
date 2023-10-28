2f775cae16a2d15f6e74e3d9b4e611a45cd48781
Rafael Villagomez <102181526+RafaelVillagomezDev@users.noreply.github.com>
2023-10-28 23:13:00 +0200

    Fix remove

--- services/app/controllers/surveyController.js
+++ services/app/controllers/surveyController.js
@@ -1,16 +1,15 @@
 const surveyService = require("../services/surveyServices");
-const productService=require("../services/productService");
-const subproductService=require("../services/subproductService");
-const userSurveyService=require("../services/userSurveyService");
+const productService = require("../services/productService");
+const subproductService = require("../services/subproductService");
+const userSurveyService = require("../services/userSurveyService");
 const db = require("../connection/bd");
-const {matchedData,validationResult} = require('express-validator');
+const { matchedData, validationResult } = require("express-validator");
 const promisePool = db.pool.promise();
 const { handleHttpError } = require("../utils/handleError");
-const { v4:uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
-const getSurvey = async (req, res,next) => {
+const getSurvey = async (req, res, next) => {
   try {
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(422).json({ errors: errors.array() });
@@ -19,106 +18,116 @@ const getSurvey = async (req, res,next) => {
     const query = surveyService.getSurvey();
     const result = await promisePool.query(query);
     res.send({
-       status:200,
-       data:result[0]
+      status: 200,
+      data: result[0],
     });
   } catch (error) {
-    
-    handleHttpError(res,"Error al obtener encuestas")
-    return
+    handleHttpError(res, "Error al obtener encuestas");
+    return;
   }
 };
 
-
-
-const createSurvey=async (req, res,next) => {
+const createSurvey = async (req, res, next) => {
   try {
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(422).json({ errors: errors.array() });
       return;
     }
 
-    const dataProvidedToken={
-      email:req.email,
-      rol:req.rol
-   }
-  
+    const dataProvidedToken = {
+      email: req.email,
+      rol: req.rol,
+    };
+
     req = matchedData(req);
-    
-   
 
-    const uuid_survey=await uuidv4()
-    const newReq={...req,id_encuesta:uuid_survey}
-    const { id_encuesta,dni, producto, mantenimiento, tipo_mantenimiento,estado,id_subproducto} = newReq;
+    const uuid_survey = await uuidv4();
+    const newReq = { ...req, id_encuesta: uuid_survey };
+    const {
+      id_encuesta,
+      dni,
+      producto,
+      mantenimiento,
+      tipo_mantenimiento,
+      estado,
+      id_subproducto,
+    } = newReq;
 
+    const queryProduct = productService.existProduct();
+    const [existProduct] = await promisePool.query(queryProduct, [producto]);
 
-    const queryProduct=productService.existProduct()
-    const [existProduct]= await promisePool.query(queryProduct, [producto]);
-    
     if (existProduct.length == 0) {
       handleHttpError(res, "ERROR AL INSERTAR PRODUCTO REVISAR BD", 401);
       return;
     }
 
-    const querySubproduct=subproductService.existSubproduct()
+    const querySubproduct = subproductService.existSubproduct();
 
-    const [existSubproduct]= await promisePool.query(querySubproduct, [id_subproducto,producto]);
-    
+    const [existSubproduct] = await promisePool.query(querySubproduct, [
+      id_subproducto,
+      producto,
+    ]);
 
-    if (existSubproduct.length == 0 && id_subproducto!="" ) {
+    if (existSubproduct.length == 0 && id_subproducto != "") {
       handleHttpError(res, "ERROR AL INSERTAR SUBPRODUCTO REVISAR BD", 401);
       return;
     }
 
-
     const queryCreate = surveyService.createSurvey();
-     
+
     const queryCreate2 = surveyService.createSurvey2();
-    let result=""
+    let result = "";
 
-    if(id_subproducto==""){
-    result = await promisePool.query(queryCreate2,[id_encuesta,dni,producto,mantenimiento,tipo_mantenimiento,estado]);
-    
-    }else{
-      result = await promisePool.query(queryCreate,[id_encuesta,dni,producto,mantenimiento,tipo_mantenimiento,estado,id_subproducto]);
+    if (id_subproducto == "") {
+      result = await promisePool.query(queryCreate2, [
+        id_encuesta,
+        dni,
+        producto,
+        mantenimiento,
+        tipo_mantenimiento,
+        estado,
+      ]);
+    } else {
+      result = await promisePool.query(queryCreate, [
+        id_encuesta,
+        dni,
+        producto,
+        mantenimiento,
+        tipo_mantenimiento,
+        estado,
+        id_subproducto,
+      ]);
     }
-   
-    
 
-    const uuid_user_survey=await uuidv4()
-    const queryUserServiceCreate=userSurveyService.createUserSurvey();
-    const resultUserService=await promisePool.query(queryUserServiceCreate,[uuid_user_survey,dataProvidedToken.email,id_encuesta]);
-  
+    const uuid_user_survey = await uuidv4();
+    const queryUserServiceCreate = userSurveyService.createUserSurvey();
+    const resultUserService = await promisePool.query(queryUserServiceCreate, [
+      uuid_user_survey,
+      dataProvidedToken.email,
+      id_encuesta,
+    ]);
+
     if (resultUserService.length == 0) {
       handleHttpError(res, "ERROR AL INSERTAR USER_SURVEY REVISAR BD", 401);
       return;
     }
 
-
-    if(result[0].affectedRows>0){
+    if (result[0].affectedRows > 0) {
       res.send({
-        status:200,
-        data:{
-          msg:"Encuesta borrada satisfactoriamente",
-          id_encuesta:id_encuesta
-        }
-     });
+        status: 200,
+        data: "Encuesta creada satifactoriamente",
+      });
     }
-  
   } catch (error) {
-    console.log(error)
-    handleHttpError(res,"Error al crear encuestas")
-    return
+    console.log(error);
+    handleHttpError(res, "Error al crear encuestas");
+    return;
   }
 };
 
-
-
-const deleteSurvey=async (req, res,next) => {
+const deleteSurvey = async (req, res, next) => {
   try {
-    
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(422).json({ errors: errors.array() });
@@ -126,58 +135,58 @@ const deleteSurvey=async (req, res,next) => {
     }
 
     req = matchedData(req);
-    const {id_encuesta}=req 
+    const { id_encuesta } = req;
 
-    const queryDelUserSurvey=userSurveyService.deleteUserSurvey()
+    const queryDelUserSurvey = userSurveyService.deleteUserSurvey();
 
-    const [deleteUserSurvey]= await promisePool.query(queryDelUserSurvey, [id_encuesta]);
-    
-    if ( deleteUserSurvey.affectedRows== 0) {
+    const [deleteUserSurvey] = await promisePool.query(queryDelUserSurvey, [
+      id_encuesta,
+    ]);
+
+    if (deleteUserSurvey.affectedRows == 0) {
       handleHttpError(res, "ERROR AL BORRAR USER ENCUESTA", 401);
       return;
     }
 
-    
-    const queryDeleteSurvey=surveyService.deleteSurvey()
+    const queryDeleteSurvey = surveyService.deleteSurvey();
 
-    const [deleteSurvey]= await promisePool.query(queryDeleteSurvey, [id_encuesta]);
-    
-    if ( deleteSurvey.affectedRows== 0) {
+    const [deleteSurvey] = await promisePool.query(queryDeleteSurvey, [
+      id_encuesta,
+    ]);
+
+    if (deleteSurvey.affectedRows == 0) {
       handleHttpError(res, "ERROR AL BORRAR ENCUESTA", 401);
       return;
     }
-    
+
     res.send({
-       status:200,
-       id:id_encuesta,
-       data:"Encuesta borrada satifactoriamente"
+      status: 200,
+      id: id_encuesta,
+      data: { msg: "Encuesta borrada satifactoriamente", id: id_encuesta },
     });
-    
   } catch (error) {
-    
-    handleHttpError(res,"Error al eliminar encuestas")
-    return
+    handleHttpError(res, "Error al eliminar encuestas");
+    return;
   }
 };
 
-const updateSurvey=async (req, res,next) => {
+const updateSurvey = async (req, res, next) => {
   try {
-
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {6
+    if (!errors.isEmpty()) {
+      6;
       res.status(422).json({ errors: errors.array() });
       return;
     }
     const query = surveyService.getSurveys();
     const result = await promisePool.query(query);
     res.send({
-       status:200,
-       data:result[0]
+      status: 200,
+      data: result[0],
     });
   } catch (error) {
-    
-    handleHttpError(res,"Error al obtener encuestas")
-    return
+    handleHttpError(res, "Error al obtener encuestas");
+    return;
   }
 };
 
