f5c335e8545db1f9efa22f9c97cd04d5f51d6c80
Rafael Villagomez <102181526+RafaelVillagomezDev@users.noreply.github.com>
2024-08-28 00:54:08 +0200

    Rm msg

--- services/app/app.js
+++ services/app/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
 
-//enpoint : http://localhost:3000/api/v1/workouts/patata//
+
 
 dotenv.config()
 
